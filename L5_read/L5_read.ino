3a37b2041fc9a33bc6406c304ef6648e95a33738
Peter Dalmaris <peter@futureshock.com.au>
2014-10-02 14:00:17 +1000

    Fixed variable name.

--- L5_read/L5_read.ino
+++ L5_read/L5_read.ino
@@ -1,4 +1,4 @@
-int incomingByte = 0;	// for incoming serial data
+char incomingChar;	// for incoming serial data
 
 void setup() {
 	Serial.begin(9600);	// opens serial port, sets data rate to 9600 bps
@@ -9,10 +9,10 @@ void loop() {
 	// send data only when you receive data:
 	if (Serial.available() > 0) {
 		// read the incoming byte:
-		incomingByte = Serial.read();
+		incomingChar = Serial.read();
 
 		// say what you got:
 		Serial.print("I received: ");
-		Serial.println(incomingByte, DEC);
+		Serial.println(incomingChar);
 	}
 }
